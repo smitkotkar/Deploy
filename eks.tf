e68319a37bf79d7d2a499b186da75f1686e5c475
smit <smitkotkar42@gmail.com>
2024-02-02 17:06:42 +0530

    df

--- eks.tf
+++ eks.tf
@@ -1,21 +1,39 @@
 provider "aws" {
-  region = "us-east-2"  # Change to your desired region
+  region = "your-region"
 }
 
-module "eks_cluster" {
+module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "my-eks-cluster"
-  subnets         = ["subnet-0d77d197c6bdb1669", "subnet-096f24687f167fa13", "subnet-0eb8d587e9a4e8051"]  # Update with your subnet IDs
+  subnets         = ["subnet-0d77d197c6bdb1669", "subnet-096f24687f167fa13", "subnet-0eb8d587e9a4e8051"]
+  vpc_id          = "vpc-0bf146a55b2b621eb"
   cluster_version = "1.21"
-  vpc_id          = "vpc-0bf146a55b2b621eb"  # Update with your VPC ID
-
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 2
-      min_capacity     = 1
-
-      instance_type = "t2.micro"   # Update with your desired instance type
-    }
-  }
 }
+
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+}
+
+
+
+# provider "aws" {
+#   region = "us-east-2"  # Change to your desired region
+# }
+
+# module "eks_cluster" {
+#   source          = "terraform-aws-modules/eks/aws"
+#   cluster_name    = "my-eks-cluster"
+#   subnets         = ["subnet-0d77d197c6bdb1669", "subnet-096f24687f167fa13", "subnet-0eb8d587e9a4e8051"]  # Update with your subnet IDs
+#   cluster_version = "1.21"
+#   vpc_id          = "vpc-0bf146a55b2b621eb"  # Update with your VPC ID
+
+#   node_groups = {
+#     eks_nodes = {
+#       desired_capacity = 2
+#       max_capacity     = 2
+#       min_capacity     = 1
+
+#       instance_type = "t2.micro"   # Update with your desired instance type
+#     }
+#   }
+# }
